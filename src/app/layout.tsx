61e9f05318367cc6cf18cd2b1020cd36b519fa5f
Khoi Nguyen <khoi.nguyen@advocado.me>
2023-11-30 20:37:34 +0700

    add typography styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Product Feedback",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={jost.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
